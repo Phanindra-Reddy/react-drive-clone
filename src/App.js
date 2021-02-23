0e31e07ffc5a85a6623babb4c53f3804e91cef46
Phanindra-Reddy <mpr4070@users.noreply.github.com>
2021-02-23 20:09:53 +0530

    Google Drive Clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import SignUp from './components/authentication/SignUp';
+import Profile from './components/Profile';
+import LogIn from './components/authentication/LogIn';
+import ForgotPassword from './components/authentication/ForgotPassword';
+import UpdateProfile from './components/authentication/UpdateProfile';
+import {AuthProvider} from './components/authentication/AuthContext';
+import PrivateRoute from './components/authentication/PrivateRoute';
+import Dashboard from './components/dashboard/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <AuthProvider>
+          <Switch>
+
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute exact path="/folder/:folderId" component={Dashboard} />
+
+            <PrivateRoute path="/user" component={Profile}/>
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+
+            <Route path="/signup" component={SignUp}/>
+            <Route path="/login" component={LogIn}/>
+            <Route path="/forgot-password" component={ForgotPassword}/>
+            
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </>
   );
 }
 
